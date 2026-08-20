f5bb99bd34ba663ff4e1539ae0ec67e3fa05d018
Kevin Manuel Omonte Roa <kev.omonteroa@gmail.com>
2026-08-20 12:51:10 -0300

    refactor(store): add context support to TaxStore
    - implement context support to TaxStore interface.
    - use sql *Context methods across all queries.
    - fix missing Rate scan in query methods.
    - fix missing verification in SearchByName method. Now it returns an error if interrupted mid scan.

--- backend/internal/store/tax_store.go
+++ backend/internal/store/tax_store.go
@@ -1,18 +1,19 @@
 package store
 
 import (
+	"context"
 	"database/sql"
 
 	"github.com/komonte/Project-ElmanPOS/backend/internal/model"
 )
 
 type TaxStore interface {
-	Create(tax *model.Tax) (*model.Tax, error)
-	GetAll() ([]*model.Tax, error)
-	GetByID(id int64) (*model.Tax, error)
-	SearchByName(query string) ([]*model.Tax, error)
-	Update(id int64, brand *model.Tax) (*model.Tax, error)
-	Delete(id int64) error
+	Create(ctx context.Context, tax *model.Tax) (*model.Tax, error)
+	GetAll(ctx context.Context) ([]*model.Tax, error)
+	GetByID(ctx context.Context, id int64) (*model.Tax, error)
+	SearchByName(ctx context.Context, query string) ([]*model.Tax, error)
+	Update(ctx context.Context, id int64, tax *model.Tax) (*model.Tax, error)
+	Delete(ctx context.Context, id int64) error
 }
 
 type taxStore struct {
@@ -24,20 +25,19 @@ func NewTaxStore(db *sql.DB) TaxStore {
 }
 
 // Create all functions to fulfill the interface
-//
-func (s *taxStore) Create(tax *model.Tax) (*model.Tax, error) {
+func (s *taxStore) Create(ctx context.Context, tax *model.Tax) (*model.Tax, error) {
 	q := `INSERT INTO taxes(name, rate) VALUES ($1, $2) RETURNING id;`
-	err := s.db.QueryRow(q, tax.Name, tax.Rate).Scan(&tax.ID)
+	err := s.db.QueryRowContext(ctx, q, tax.Name, tax.Rate).Scan(&tax.ID)
 	if err != nil {
 		return nil, err
 	}
 	return tax, nil
 }
 
-func (s *taxStore) GetAll() ([]*model.Tax, error) {
+func (s *taxStore) GetAll(ctx context.Context) ([]*model.Tax, error) {
 	q := `SELECT id, name, rate FROM taxes;`
 
-	rows, err := s.db.Query(q)
+	rows, err := s.db.QueryContext(ctx, q)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +46,7 @@ func (s *taxStore) GetAll() ([]*model.Tax, error) {
 	taxes := make([]*model.Tax, 0)
 	for rows.Next() {
 		t := new(model.Tax)
-		if err := rows.Scan(&t.ID, &t.Name); err != nil {
+		if err := rows.Scan(&t.ID, &t.Name, &t.Rate); err != nil {
 			return nil, err
 		}
 
@@ -60,12 +60,12 @@ func (s *taxStore) GetAll() ([]*model.Tax, error) {
 	return taxes, nil
 }
 
-func (s *taxStore) GetByID(id int64) (*model.Tax, error) {
+func (s *taxStore) GetByID(ctx context.Context, id int64) (*model.Tax, error) {
 	q := `SELECT id, name, rate FROM taxes WHERE id = $1;`
 
 	var t model.Tax
 
-	err := s.db.QueryRow(q, id).Scan(&t.ID, &t.Name)
+	err := s.db.QueryRowContext(ctx, q, id).Scan(&t.ID, &t.Name, &t.Rate)
 	if err != nil {
 		return nil, err
 	}
@@ -73,30 +73,33 @@ func (s *taxStore) GetByID(id int64) (*model.Tax, error) {
 	return &t, nil
 }
 
-func (s *taxStore) SearchByName(query string) ([]*model.Tax, error) {
+func (s *taxStore) SearchByName(ctx context.Context, query string) ([]*model.Tax, error) {
 	q := `SELECT id, name, rate FROM taxes WHERE name ILIKE '%' || $1 || '%' ORDER BY name ASC LIMIT 20;`
-	rows, err := s.db.Query(q, query)
+	rows, err := s.db.QueryContext(ctx, q, query)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var taxes []*model.Tax
+	taxes := make([]*model.Tax, 0)
 	for rows.Next() {
 		t := new(model.Tax)
-		if err := rows.Scan(&t.ID, &t.Name); err != nil {
+		if err := rows.Scan(&t.ID, &t.Name, &t.Rate); err != nil {
 			return nil, err
 		}
 
 		taxes = append(taxes, t)
 	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
 	return taxes, nil
 }
 
-func (s *taxStore) Update(id int64, tax *model.Tax) (*model.Tax, error) {
+func (s *taxStore) Update(ctx context.Context, id int64, tax *model.Tax) (*model.Tax, error) {
 	q := `UPDATE taxes SET name = $1, rate = $2 WHERE id = $3;`
 
-	res, err := s.db.Exec(q, tax.Name, tax.Rate, id)
+	res, err := s.db.ExecContext(ctx, q, tax.Name, tax.Rate, id)
 	if err != nil {
 		return nil, err
 	}
@@ -115,10 +118,10 @@ func (s *taxStore) Update(id int64, tax *model.Tax) (*model.Tax, error) {
 	return tax, nil
 }
 
-func (s *taxStore) Delete(id int64) error {
+func (s *taxStore) Delete(ctx context.Context, id int64) error {
 	q := `DELETE from taxes WHERE id = $1;`
 
-	res, err := s.db.Exec(q, id)
+	res, err := s.db.ExecContext(ctx, q, id)
 	if err != nil {
 		return err
 	}

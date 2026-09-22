530ddcafbf9fc0cec95362d5f7642e0f3722e2fa
Kevin Manuel Omonte Roa <kev.omonteroa@gmail.com>
2026-09-21 21:00:10 -0300

    feat(model): add rich domain validations to Brand

--- backend/internal/model/brand.go
+++ backend/internal/model/brand.go
@@ -1,7 +1,49 @@
 // Package model defines domain entities, data structures, and types for ElmanPOS.
 package model
 
+import (
+	"errors"
+	"strings"
+	"unicode/utf8"
+)
+
+const (
+	BrandNameMinLen = 2
+	BrandNameMaxLen = 120
+)
+
+var (
+	ErrNilBrand = errors.New("brand cannot be nil")
+
+	ErrEmptyBrandName = errors.New("brand name is required")
+	ErrBrandNameTooShort = errors.New("brand name must be at least 2 characters")
+	ErrBrandNameTooLong  = errors.New("brand name cannot exceed 120 characters")
+)
+
 type Brand struct {
 	ID int64 `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
+}
+
+
+func (b *Brand) Validate() error {
+	if b == nil {
+		return ErrNilBrand
+	}
+
+
+	b.Name = strings.ToUpper(strings.TrimSpace(b.Name))
+	if b.Name == "" {
+		return ErrEmptyBrandName
+	}
+
+	nameLen := utf8.RuneCountInString(b.Name)
+	if nameLen < BrandNameMinLen {
+		return ErrBrandNameTooShort
+	}
+	if nameLen > BrandNameMaxLen {
+		return ErrBrandNameTooLong
+	}
+
+	return nil
 }
